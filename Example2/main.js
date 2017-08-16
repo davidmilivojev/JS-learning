87232a68301583172080cdb015a5026849397c2a
david milivojev <davyd7_@hotmail.com>
2017-08-16 13:20:02 +0200

    fix game bugs

--- Example2/main.js
+++ Example2/main.js
@@ -1,4 +1,5 @@
 var btn1 = document.getElementById('btn1');
+//var btn2 = document.getElementById('btn2');
 
 var score1 = document.getElementById('score1');
 var score2 = document.getElementById('score2');
@@ -14,24 +15,34 @@ if(localStorage.getItem('Pobednik') == '1'){
   alert('trenutno nema pobednika');
 }
 
-btn1.addEventListener('click', player1);
+btn1.addEventListener('mousedown', player1);
 
-function player1(){
-  i++;
-  score1.innerHTML = i;
-  if(i == 20){
-    alert('pobednik je player 1');
-    localStorage.setItem('Pobednik','1');
+function player1(event){
+  if(event.which == 1){
+    console.log('left');
+    i++;
+    score1.innerHTML = i;
+    if(i == 20){
+      alert('pobednik je player 1');
+      localStorage.setItem('Pobednik','1');
+    }
   }
+
 }
 
 addEventListener('keydown', player2);
 
-function player2(){
-  d++;
-  score2.innerHTML = d;
-  if(d == 20){
-    alert('pobednik je player 2');
-    localStorage.setItem('Pobednik','2');
+function player2(event){
+  var gKey = event.key;
+  var spaceKey = event.keyCode;
+
+  if(gKey == 'g' || spaceKey == '32'){
+    d++;
+    score2.innerHTML = d;
+    if(d == 20){
+      alert('pobednik je player 2');
+      localStorage.setItem('Pobednik','2');
+    }
   }
+
 }
